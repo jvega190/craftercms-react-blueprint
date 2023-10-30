768afd8a31666a9575d452f05c5b29831157c088
jvega190 <josevega19@gmail.com>
2023-10-30 12:15:46 -0600

    Fix image rendering, avoid image spinning when editMode is on

--- sources/packages/app/src/App.tsx
+++ sources/packages/app/src/App.tsx
@@ -1,10 +1,51 @@
 import React, { Suspense } from 'react';
-import { ExperienceBuilder, RenderField } from '@craftercms/experience-builder/react';
+import { ExperienceBuilder, RenderField, useGuestContext } from '@craftercms/experience-builder/react';
 import { ContentInstance } from '@craftercms/models';
 import { getModel } from './lib/api';
 import { BASE_URL } from './constants';
 import { isAuthoring } from './utils';
 import './App.css';
+
+interface ContentProps {
+  model: ContentInstance;
+}
+
+function Header(props: ContentProps) {
+  const { model } = props;
+  const guestContext = useGuestContext();
+  const editMode = guestContext?.editMode;
+
+  return (
+    <header className="App-header">
+      <RenderField model={model} fieldId="headText_s" component="p" />
+      <RenderField
+        model={model}
+        fieldId="logo_s"
+        renderTarget="src"
+        component="img"
+        componentProps={{
+          className: `App-logo ${editMode ? '' : 'App-logo-spin'}`,
+          alt: 'logo'
+        }}
+        render={(logo) => `${BASE_URL}${logo}`}
+      />
+      <RenderField model={model} fieldId="subtitle_s" component="p" />
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
+      <div>
+        <div className="App-note">
+          <RenderField
+            model={model}
+            fieldId="body_html"
+            renderTarget="dangerouslySetInnerHTML"
+            render={(value) => ({ __html: value })}
+          />
+        </div>
+      </div>
+    </header>
+  );
+}
 
 function App() {
   const [model, setModel] = React.useState<ContentInstance>();
@@ -20,28 +61,7 @@ function App() {
       <div className="App" role="main">
         {model && (
           <ExperienceBuilder isAuthoring={isAuthoring()} path={model.craftercms?.path}>
-            <header className="App-header">
-              <RenderField model={model} fieldId="headText_s" component="p" />
-              <RenderField
-                model={model}
-                fieldId="logo_s"
-                render={(logo) => <img src={`${BASE_URL}${logo}`} className="App-logo" alt="logo" />}
-              />
-              <RenderField model={model} fieldId="subtitle_s" component="p" />
-              <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                Learn React
-              </a>
-              <div>
-                <div className="App-note">
-                  <RenderField
-                    model={model}
-                    fieldId="body_html"
-                    renderTarget="dangerouslySetInnerHTML"
-                    render={(value) => ({ __html: value })}
-                  />
-                </div>
-              </div>
-            </header>
+            <Header model={model} />
           </ExperienceBuilder>
         )}
       </div>
