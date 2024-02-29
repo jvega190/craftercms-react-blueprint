073c53686dd21dc09d847dd72ff795d35b09d815
jvega190 <josevega19@gmail.com>
2024-02-29 15:40:40 -0600

    Revert getDescriptor update

--- sources/packages/app/src/lib/api.ts
+++ sources/packages/app/src/lib/api.ts
@@ -1,8 +1,9 @@
-import {getItem, parseDescriptor} from '@craftercms/content';
-import {map} from 'rxjs';
+import { getDescriptor, parseDescriptor } from '@craftercms/content';
+import { DescriptorResponse } from '@craftercms/models';
+import { map } from 'rxjs';
 
 export function getModel(path = '/site/website/index.xml') {
-  return getItem(path).pipe(
-    map((item) => parseDescriptor(item.descriptorDom))
+  return getDescriptor(path, { flatten: true }).pipe(
+    map((descriptor: DescriptorResponse | DescriptorResponse[]) => parseDescriptor(descriptor))
   );
 }
